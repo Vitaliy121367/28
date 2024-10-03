61f5571912eae46ccdce79bf1f34d83905500483
Vitaliy <qw929496@gmail.com>
2024-10-03 16:35:17 +0300

    Scanner cpp

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,1 +1,36 @@
 #include "Scanner.h"
+
+Scanner::Scanner()
+{
+    text = "emty";
+}
+
+void Scanner::setText(string text)
+{
+    this->text = text;
+}
+
+string Scanner::getText() const
+{
+    return text;
+}
+
+void Scanner::scanFile(string fileName)
+{
+    ifstream file(fileName);
+    if (file.is_open())
+    {
+        string temp;
+        text = "";
+        while (!file.eof())
+        {
+            getline(file, temp);
+            text += temp + "\n";
+        }
+        file.close();
+        cout << "Text was scan\n\n";
+    }
+    else {
+        cout << "Error\n\n";
+    }
+}
--- Scanner.h
+++ Scanner.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <fstream>
+#include <iostream>
 using namespace std;
 class Scanner
 {
