f5ab978dea2a6925f62e371a94cc5c3dec663a3b
Vitaliy <qw929496@gmail.com>
2024-10-03 16:25:03 +0300

    Add project files.

---
+++ 28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+#include <stdio.h>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+
+    return 0;
+}
